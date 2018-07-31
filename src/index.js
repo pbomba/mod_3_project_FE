56f9b64f09106f53fe86d9e65d44a6b5e9ef2b79
pbomba <paul@paulbomba.com>
2018-07-31 13:02:01 -0400

    worked out logic for song playback... super slow

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ document.addEventListener('click', clickHandler)
 let eventArray = [[], [], [], []]
 let rec_start = 0
 let recording_track = 0
+let current_track = 0
 
 function setup() {
 	sounds = {
@@ -21,6 +22,7 @@ function clickHandler(e) {
 	if (e.target.className === "sound") {
 		// if we are "recording"
 		if (recording_track > 0){
+			
 		e.preventDefault()
 		sounds[e.target.id].play()
 		eventItem = e.target.id
@@ -40,16 +42,24 @@ function clickHandler(e) {
 	eventObj = {sound: eventItem, time: eventTime}
 	eventArray[recording_track].push(eventObj)
 	// if the button is "play"
-	} else if (e.target.className === "play_all"){
-		soundTimesArray = mapArray()
-		playMusic(soundTimesArray)
+	} else if (e.target.className === "play"){
+	current_track = parseInt(e.target.dataset.id)
+		if (e.target.id === "play_all") {
+			// play the entire song
+			let mergedTrack = mergeTracks()
+			playTrack(mergedTrack)
+		} else {
+			// play an individual track
+		soundTimesArray = mapArray(current_track)
+		playTrack(soundTimesArray)
+		}
 	}
 }
 
 function startRecording (timeStamp, track) {
 	eventArray[track] = []
 	rec_start = timeStamp
-	rec_btn = document.querySelector('#record_1')
+	rec_btn = document.querySelector(`#record_${track}`)
 	rec_btn.disabled = true
 	rec_btn.innerText = "Stop"
 	setTimeout(resetRec, 10000)
@@ -60,25 +70,41 @@ rec_btn.disabled = false
 rec_btn.innerText = "Record"
 }
 
-function mapArray() {
-	return eventArray.map(function(obj){
+function mapArray(track) {
+	track_time = eventArray[track][0].time
+	return eventArray[track].map(function(obj){
 		return {
 			sound: obj.sound,
-			time: obj.time - rec_start
+			time: obj.time - track_time
 		}
 	})
 }
 
-function playMusic(soundTimesArray) {
+function playTrack(soundTimesArray) {
 	soundTimesArray.shift()
 	soundTimesArray.forEach( event => setTimeout( () => playSound(event.sound), event.time))
+	// debugger
 	 // soundTimesArray.forEach( event => console.log(event))
 	}
 
 function playSound(eventSound){
+	//eventSound is undefined here
 	sounds[eventSound].play()
 }
 
+function mergeTracks() {
+	let zeroArray = [[]]
+	let track1 = eventArray[1]
+	track1.shift()
+	let track2 = eventArray[2]
+	track2.shift()
+	let track3 = eventArray[3]
+	track3.shift()
+	let merger = [...track1, ...track2, ...track3]
+	merger.sort( (a, b) => a.time - b.time)
+	let mergedWithZero = [...zeroArray,...merger ]
+	return mergedWithZero
+}
 
 // eventArray.forEach (function (i) {
 // 	console.log(Object.keys(i))
